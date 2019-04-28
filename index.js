793848f573607ce650c2d79bd610138a56ca3291
Florian Westreicher <florian.westreicher@rise-world.com>
2019-04-28 16:27:35 +0200

    login and group join working
    closes #1
    closes #2

--- index.js
+++ index.js
@@ -10,39 +10,26 @@
 
     */
     /* globals app, socket */
-    var user = module.parent.require('./user'),
-        meta = module.parent.require('./meta'),
-        db = module.parent.require('./database'),
-        winston = module.parent.require('winston'),
-        passport = module.parent.require('passport'),
-        fs = module.parent.require('fs'),
-        path = module.parent.require('path'),
-        nconf = module.parent.require('nconf'),
-        async = module.parent.require('async'),
-        local_strategy = module.parent.require('passport-local').Strategy,
+    var user = require.main.require('./src/user'),
+        Groups = require.main.require('./src/groups'),
+        meta = require.main.require('./src/meta'),
+        db = require.main.require('./src/database'),
+        winston = require.main.require('winston'),
+        passport = require.main.require('passport'),
+        async = require.main.require('async'),
+        // fs = module.parent.require('fs'),
+        // path = module.parent.require('path'),
+        // nconf = module.parent.require('nconf'),
+        local_strategy = require.main.require('passport-local').Strategy,
         ldapjs = require('ldapjs');
     const Hashes = require('jshashes');
 
     var master_config = {};
+    var global_ldap_options = {};
     var open_ldap = {
         name: "OpenLDAP",
 
-        get_domain: function (base) {
-            var domain = '';
-            if (base !== '') {
-                var temp = base.match(/dc=([^,]*)/gi);
-                if (temp && temp.length > 0) {
-                    domain = temp.map(function (str) {
-                        return str.match(/dc=([^,]*)/i)[1];
-                    }).reduce(function (current, previous) {
-                        return current + '.' + previous;
-                    });
-                }
-            }
-            return domain;
-        },
-
-        admin: function (custom_header, callback) {
+        adminHeader: function (custom_header, callback) {
             custom_header.plugins.push({
                 "route": "/plugins/open_ldap",
                 "icon": "fa-cog",
@@ -51,44 +38,56 @@
             callback(null, custom_header);
         },
 
-        init: function (params, callback) {
-            function render(req, res, next) {
-                res.render('open_ldap', {});
-            }
-
-            meta.settings.get('openldap', function (err, options) {
-                master_config = options;
-            });
-            params.router.get('/admin/plugins/open_ldap', params.middleware.admin.buildHeader, render);
-            params.router.get('/api/admin/plugins/open_ldap', render);
-
-            callback();
-        },
-
-        get_config: function (options, callback) {
+        getConfig: function (options, callback) {
+            winston.info("get config");
             meta.settings.get('openldap', function (err, settings) {
                 if (err) {
-                    return callback(null, options);
+                    return callback(err);
                 }
-                master_config = settings;
                 options.openldap = settings;
                 callback(null, options);
             });
         },
 
-        fetch_config: function (callback) {
-            meta.settings.get('openldap', function (err, options) {
-                callback(options);
-            });
-        },
+        init: function (params, callback) {
+            winston.info("openldap init");
+            function render(req, res, next) {
+                res.render('open_ldap', {});
+            }
+
+            params.router.get('/admin/plugins/open_ldap', params.middleware.admin.buildHeader, render);
+            params.router.get('/api/admin/plugins/open_ldap', render);
+
+            const defaultOptions = {
+                server: "ldap://172.17.0.3",
+                port: "",
+                base: "dc=example,dc=org",
+                admin_user: "cn=admin,dc=example,dc=org",
+                password: "admin",
+                user_query: "(&(|(objectclass=inetOrgPerson))(uid=%uid))",
+                groups_query: "(&(|(objectclass=posixGroup)))",
+                admin_groups: "admins",
+                moderator_groups: "mods"
+            };
 
 
-        stringtoint: function (str) {
-            return str.split('').map(function (char) {
-                return char.charCodeAt(0);
-            }).reduce(function (current, previous) {
-                return previous + current;
-            });
+            async.waterfall([
+                function (next) {
+                    open_ldap.getConfig({}, function (err, config) {
+                        if (err) {
+                            return next(err);
+                        }
+                        master_config = config.openldap.server ? config.openldap : defaultOptions;
+                        global_ldap_options.url = master_config.server + ':' + master_config.port
+                        winston.info("master_config: " + JSON.stringify(master_config));
+                        next();
+                    });
+                },
+                open_ldap.findLdapGroups,
+                function (groups, next) {
+                    async.each(groups, open_ldap.createGroup, next);
+                }
+            ], callback);
         },
 
         override: function () {
@@ -101,64 +100,102 @@
                 if (!password) {
                     return next(new Error('[[error:invalid-password]]'));
                 }
-                if (typeof master_config.server === 'undefined') {
-                    open_ldap.fetch_config(function (config) {
-                        var options = {
-                            url: config.server + ':' + config.port
-                        };
-                        master_config = config;
-                        open_ldap.process(options, username, password, next);
-                    });
-                } else {
-                    var options = {
-                        url: master_config.server + ':' + master_config.port
-                    };
-                    open_ldap.process(options, username, password, next);
-                }
+                open_ldap.process(username, password, next);
             }));
         },
 
-        process: function (options, username, password, next) {
-            try {
-                var adminClient = ldapjs.createClient(options);
-                winston.info("client created with master_config: " + JSON.stringify(master_config));
-                // try user login first
-                adminClient.bind(master_config.admin_user, master_config.password, function (err) {
+
+        findLdapGroups: function (callback) {
+            winston.info("load groups");
+            open_ldap.adminClient(function (err, adminClient) {
+                if (err) {
+                    return callback(err);
+                }
+                var groups_search = {
+                    filter: master_config.groups_query,
+                    scope: 'sub',
+                    attributes: ['cn', 'memberUid']
+                };
+
+                adminClient.search(master_config.base, groups_search, function (err, res) {
+                    let groups = [];
                     if (err) {
-                        winston.error("admin login failed, check your config" + err.message);
-                        return next(new Error('[[error:invalid-email]]')); // we don't leak information
+                        return callback(new Error('groups could not be found'));
                     }
-                    // client.bind(master_config.user_query.replace("%uid", username), password, function (err) {
+                    res.on('searchEntry', function (entry) {
+                        const group = entry.object;
+                        groups.push(group)
+                    });
+                    res.on('end', function () {
+                        adminClient.unbind();
+                        callback(null, groups);
+                    });
+                });
+            });
+        },
+
+        adminClient: function (callback) {
+            var client = ldapjs.createClient(global_ldap_options);
+            client.bind(master_config.admin_user, master_config.password, function (err) {
+                if (err) {
+                    return callback(new Error('could not bind with admin config ' + err.message));
+                }
+                callback(null, client);
+            });
+        },
+
+        createGroup: function (ldapGroup, callback) {
+            // creates the group 
+            const groupName = "ldap-" + ldapGroup.cn;
+            winston.info("create group " + groupName);
+            const groupData = {
+                name: groupName,
+                userTitleEnabled: false,
+                description: 'LDAP Group ' + ldapGroup.cn,
+                // hidden: true,
+                // system: true,
+                // private: true,
+                disableJoinRequests: true,
+            };
+            Groups.create(groupData, function (err, group) {
+                callback(null, groupName);
+            });
+        },
+
+        process: function (username, password, next) {
+            try {
+                open_ldap.adminClient(function (err, adminClient) {
                     if (err) {
-                        winston.error(err.message);
-                        return next(new Error('[[error:invalid-email]]')); // we don't leak information
+                        return next(err);
                     }
                     var opt = {
                         filter: master_config.user_query.replace('%uid', username),
+                        sizeLimit: 1,
                         scope: 'sub',
-                        sizeLimit: 1
+                        attributes: ['dn', 'uid', 'sn', 'mail', //these fields are mandatory
+                            // optional fields. used to create the user id/fullname
+                            'givenName', 'displayName',
+                        ]
                     };
 
                     adminClient.search(master_config.base, opt, function (err, res) {
-                        winston.info("search");
                         if (err) {
-                            return next(new Error('[[error:invalid-email]]'));
+                            return next(err);
                         }
-
                         res.on('searchEntry', function (entry) {
                             var profile = entry.object;
                             winston.info('profile: ' + JSON.stringify(profile));
                             // now we check the password
-                            const userClient = ldapjs.createClient(options);
+                            const userClient = ldapjs.createClient(global_ldap_options);
                             userClient.bind(profile.dn, password, function (err) {
+                                winston.info("user authenticated");
+                                userClient.unbind();
+
                                 if (err) {
                                     return next(new Error('[[error:invalid-email]]'));
                                 }
-                                userClient.unbind();
-                                // auth worked :)
-                                var SHA512 = new Hashes.SHA512
-                                var id = SHA512.b64(profile.uid);
-                                open_ldap.login(id, profile.cn, profile.givenName + " " + profile.sn, profile.mail, function (err, userObject) {
+
+                                open_ldap.login(profile, function (err, userObject) {
                                     if (err) {
                                         winston.error(err);
                                         return next(new Error('[[error:invalid-email]]'));
@@ -167,83 +204,122 @@
                                 });
 
                             });
+
                         });
-                        res.on('searchReference', function (referral) {
-                            winston.info('referral: ' + JSON.stringify(referral));
+                        res.on('end', function (result) {
+                            adminClient.unbind();
                         });
                         res.on('error', function (err) {
+                            adminClient.unbind();
                             winston.error('OpenLDAP Error:' + err.message);
                             return next(new Error('[[error:invalid-email]]'));
                         });
+
                     });
-                    // });
                 });
             } catch (err) {
                 winston.error('OpenLDAP Error :' + err.message);
             }
         },
 
-        login: function (ldapid, fullname, username, email, callback) {
-            winston.info("doing login");
-            var _self = this;
-            _self.getuidby_ldapid(ldapid, function (err, uid) {
+        login: (profile, callback) => {
+            winston.info("doing login: " + JSON.stringify(profile));
+            // build the username
+            let fullname = profile.sn;
+            if (profile.givenName) {
+                fullname = profile.givenName + " " + fullname;
+            }
+            if (profile.displayName) {
+                fullname = profile.displayName;
+            }
+
+            open_ldap.getUserByLdapUid(profile.uid, (err, dbUser) => {
                 if (err) {
                     return callback(err);
                 }
-
-                if (uid !== null) {
-                    return callback(null, {
-                        uid: uid
-                    });
+                if (dbUser.uid !== 0) {
+                    // user exists
+                    // now we check the user groups
+                    winston.info("user exists:" + JSON.stringify(dbUser));
+                    return open_ldap.postLogin(dbUser.uid, profile.uid, callback);
                 } else {
-                    winston.info("no uid in the db")
                     // New User
-                    var success = function (uid) {
-                        // Save provider-specific information to the user
-                        user.setUserField(uid, 'ldapid', ldapid);
-                        db.setObjectField('ldapid:uid', ldapid, uid);
-                        // TODO: set groups here #2
-                        callback(null, {
-                            uid: uid
-                        });
-                    };
-
-                    return user.getUidByEmail(email, function (err, uid) {
+                    var pattern = new RegExp(/[\ ]*\(.*\)/);
+                    let username = fullname;
+                    if (pattern.test(username)) {
+                        username = username.replace(pattern, '');
+                    }
+                    winston.info("create user: " + JSON.stringify(profile))
+                    return user.create({ username: username, fullname: fullname, email: profile.mail }, function (err, uid) {
                         if (err) {
                             return callback(err);
                         }
-
-                        if (!uid) {
-                            var pattern = new RegExp(/[\ ]*\(.*\)/);
-                            if (pattern.test(username)) {
-                                username = username.replace(pattern, '');
-                            }
-                            winston.info("create user")
-                            return user.create({ username: username, fullname: fullname, email: email }, function (err, uid) {
-                                if (err) {
-                                    return callback(err);
-                                }
-                                if (master_config.autovalidate == 1) {
-                                    user.setUserField(uid, 'email:confirmed', 1);
-                                }
-                                return success(uid);
-                            });
-                        } else {
-                            return success(uid); // Existing account -- merge
-                        }
+                        user.setUserField(uid, 'email:confirmed', 1);
+                        db.setObjectField('ldapid:uid', profile.uid, uid)
+                        db.setObjectField('ldapid:ldapid', uid, profile.uid)
+                        return open_ldap.postLogin(uid, profile.uid, callback);
                     });
                 }
             });
         },
+        postLogin: function (uid, ldapId, callback) {
+            async.waterfall([
+                open_ldap.findLdapGroups,
+                (groups, next) => {
+                    async.each(groups,
+                        (ldapGroup, next) => {
+                            open_ldap.groupJoin(ldapGroup, ldapId, uid, next);
+                        }, next);
+                }],
+                () => {
+                    callback(null, { uid: uid });
+                }
+            );
+        },
 
-        getuidby_ldapid: function (ldapid, callback) {
-            db.getObjectField('ldapid:uid', ldapid, function (err, uid) {
+        groupJoin: function (ldapGroup, ldapId, uid, callback) {
+            open_ldap.createGroup(ldapGroup,
+                function (err, groupId) {
+                    if (err) {
+                        return callback(err);
+                    }
+                    let members = ldapGroup.memberUid;
+                    if (!Array.isArray(members)) {
+                        members = [members];
+                    }
+                    if (members.includes(ldapId)) {
+                        const groupsToJoin = [groupId];
+                        console.log("groupId, ldapid, uid", groupId, ldapId, uid);
+                        if (master_config.admin_groups.split(',').includes(ldapGroup.cn)) {
+                            groupsToJoin.push('administrators');
+                        }
+                        if (master_config.moderator_groups.split(',').includes(ldapGroup.cn)) {
+                            groupsToJoin.push('Global Moderators');
+                        }
+                        return Groups.join(groupsToJoin, uid, callback);
+                    }
+                    else {
+                        callback();
+                    }
+                }
+            );
+        },
+
+        getUserByLdapUid: function (ldapUid, callback) {
+            db.getObjectField('ldapid:uid', ldapUid, function (err, uid) {
                 if (err) {
                     return callback(err);
                 }
-                return callback(null, uid);
+                user.getUserData(uid, function (err, data) {
+                    if (err) {
+                        return callback(err);
+                    }
+                    winston.info("user data from db: " + JSON.stringify(data));
+                    callback(null, data);
+                });
             });
-        }
+        },
+
     };
 
     module.exports = open_ldap;
